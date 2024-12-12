6c5a411c713afb9923f32d754dcf0cb0be0e247c
David Julian Muñoz Norberto <143279636+davidjulian7@users.noreply.github.com>
2024-12-12 14:21:52 -0600

    cambios al modal de vacaciones

--- WebAppVacaciones/Pages/EmpleadoInfo.aspx.designer.cs
+++ WebAppVacaciones/Pages/EmpleadoInfo.aspx.designer.cs
@@ -31,5 +31,14 @@ namespace WebAppVacaciones.Pages
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView gridVacaciones;
+
+        /// <summary>
+        /// Control TextFechaVacaciones.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextFechaVacaciones;
     }
 }
